a82fa19d67a6185637caa80475f24c1cae4d38fd
Divinelewis <divinitysoft0@gmail.com>
2023-06-06 19:32:52 +0100

    Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers)

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Get the total number of subscribers
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    A Python function that queries reddit API to get the total number of subscribers not active userss
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Microsoft Edge Version 113.0.1774.57'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
